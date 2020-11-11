0dba4e348f51447033dbc53a4a82aae697e3836a
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2020-11-11 19:42:46 +0600

    Create 1371A - Magical Sticks.cpp

---
+++ 1371A - Magical Sticks.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+int t,x;
+cin>>t;
+while(t--){
+    cin>>x;
+    x%2==0 ? x%2==1
+    if(x%2==0){
+        cout<<x/2<<endl;
+    }else{
+        cout<<(x+1)/2<<endl;
+    }
+}
+return 0;
+}
