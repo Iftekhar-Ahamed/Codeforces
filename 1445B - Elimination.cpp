b51c847ae2ef69e8d377f28df9028329cdba7ac3
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2020-11-13 12:50:44 +0600

    Create 1445B - Elimination.cpp

---
+++ 1445B - Elimination.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main(){
+int a,b,c,d,t;
+cin>>t;
+while(t--){
+    cin>>a>>b>>c>>d;
+    cout<<max(a+b,c+d)<<endl;
+}
+
+return 0;
+}
