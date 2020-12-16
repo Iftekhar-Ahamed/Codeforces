0653510f7c10ee61bed32ec5c871e537f1a19645
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2020-12-16 12:47:21 +0600

    Create A - Special Permutation.cpp

---
+++ A - Special Permutation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n,i,t;
+cin>>t;
+while(t--){
+    cin>>n;
+    for(i=2;i<=n;i++){
+    printf("%d ",i);
+}
+printf("1\n");
+}
+
+return 0;
+}
