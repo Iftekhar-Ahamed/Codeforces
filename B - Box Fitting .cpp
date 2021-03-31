d7479cfec4bd1c8865654e4a6adb82c02d771adc
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-03-31 20:50:55 +0600

    Create B - Box Fitting .cpp

---
+++ B - Box Fitting .cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define read freopen("input.txt", "r", stdin)
+#define write freopen("output.txt", "w", stdout)
+#define MEM(a, b) memset(a, (b), sizeof(a))
+#define _for(i, j, k, in) for (int i = j; i < k; i += in)
+#define _rfor(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define _foritaretor(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define MP make_pair
+#define PB push_back
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const double pi = acos(-1.0);
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//read;write;
+	lld sum,w,temp;
+	lld t,n,i,count;
+	cin>>t;
+	while (t--)
+	{
+		cin>>n>>w;
+		sum=0,count=1;
+		multiset<lld>data;
+		for(i=0;i<n;i++){
+			cin>>temp;
+			data.insert(temp);
+		}
+		sum=w;
+		while (!data.empty())
+		{
+			auto it=data.upper_bound(sum);
+			if(it!=data.begin()){
+				sum-=*--it;
+				data.erase(it);
+			}else{
+				count++,sum=w;
+			}
+		}
+		cout<<count<<endl;
+	}
+	return 0;
+}
