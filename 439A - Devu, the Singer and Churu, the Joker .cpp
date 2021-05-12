154a22c03d062abe65d49596ac0e369b2003faf7
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-05-13 00:18:41 +0600

    Create 439A - Devu, the Singer and Churu, the Joker .cpp

---
+++ 439A - Devu, the Singer and Churu, the Joker .cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define read freopen("input.txt", "r", stdin)
+#define write freopen("output.txt", "w", stdout)
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const double pi = acos(-1.0);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    //read;
+    //write;
+    lld n,i,jock,time,finishtime,x;
+    cin>>n>>finishtime;
+    time=(n-1)*10;
+    jock=(n-1)*2;
+    for(i=0;i<n;i++){
+        cin>>x;
+        time+=x;
+    }
+    x=finishtime-time;
+    if(finishtime>=time ){
+        jock+=(x/5);
+        cout<<jock<<endl;
+    }else{
+        cout<<"-1"<<endl;
+    }
+    return 0;
+}
