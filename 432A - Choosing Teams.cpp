bb8712341f256e15881c596d96a92b74b1aa3ae3
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-05-14 23:15:55 +0600

    Create  432A - Choosing Teams.cpp

---
+++ 432A - Choosing Teams.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const double pi = acos(-1.0);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    lld n,i,k,x,c=0;
+    cin>>n>>k;
+    k=5-k;
+    for(i=0;i<n;i++){
+        cin>>x;
+        if(x<=k)c++;
+    }
+    cout<<c/3<<endl;
+    return 0;
+}
