b10abdb374664316c53b9f9d0c1614911469b3ab
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-05-18 18:02:00 +0600

    Create 703A - Mishka and Game .cpp

---
+++ 703A - Mishka and Game .cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const double pi = acos(-1.0);
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    lld n,x,y,c1=0,c2=0;
+    cin>>n;
+    while (n--)
+    {
+        cin>>x>>y;
+        if(x>y)c1++;
+        else if(x<y)c2++;
+    }
+    if(c1>c2)cout<<"Mishka\n";
+    else if(c2>c1)cout<<"Chris\n";
+    else cout<<"Friendship is magic!^^\n";
+    return 0;
+}
