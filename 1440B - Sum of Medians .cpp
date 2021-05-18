37bec5dd0a6a0c9bdccc263245661f6594d65ecb
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-05-18 19:44:26 +0600

    Create 1440B - Sum of Medians .cpp

---
+++ 1440B - Sum of Medians .cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const double pi = acos(-1.0);
+#define mxS 1000000
+int nXm[mxS];
+void solve(){
+    lld n,m,i,size,sum=0,x,y;
+    cin>>n>>m;
+    size=n*m;
+    for(i=0;i<size;i++)cin>>nXm[i];
+    y=n/2;
+    y+=1;
+    x=n-y;
+    x*=m;
+    while (x<size)
+    {
+        size-=y;
+        sum+=nXm[size];
+    }
+    cout<<sum<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    lld t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
