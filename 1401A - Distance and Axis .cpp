e5a4195eeacf45ee1b3fde7485aea9b64f803f3b
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-05-19 19:37:08 +0600

    Create 1401A - Distance and Axis .cpp

---
+++ 1401A - Distance and Axis .cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+
+using namespace std;
+
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+const double pi = acos(-1.0);
+lld solve()
+{
+    lld n, k;
+    cin >> n >> k;
+    if (n < k)
+        return k - n;
+    else if (n % 2 == k % 2)
+        return 0;
+    else
+        return 1;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    lld t;
+    cin >> t;
+    while (t--)
+    {
+        cout << solve() << endl;
+    }
+
+    return 0;
+}
