377e4be78f28b987783217dfb8f378213b7f6f54
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2021-06-24 16:03:42 +0600

    Create B - Love Song.cpp

---
+++ B - Love Song.cpp
@@ -0,0 +1,71 @@
+/***
+**      Bismillahir Rahmanir Rahim
+**              ALLAHU AKBAR
+**
+**     Author: Iftekhar Ahamed Siddiquee
+**     Bangladesh University of Business and Technology,
+**     Dept. of CSE.
+***/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <queue>
+#include <deque>
+#include <bitset>
+#include <iterator>
+#include <list>
+#include <stack>
+#include <map>
+#include <set>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <limits>
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+using namespace std;
+
+#define read freopen("input.txt", "r", stdin)
+#define write freopen("output.txt", "w", stdout)
+typedef long int int32;
+typedef unsigned long int uint32;
+typedef long long int int64;
+typedef unsigned long long int uint64;
+#define lld long long int
+#define INF (int)1e9
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define mXs 1000000
+const double pi = acos(-1.0);
+
+int main()
+{
+    cin.tie(NULL);
+    //read;
+    //write;
+    ios_base::sync_with_stdio(false);
+    lld sum, n, q, i, j;
+    char ch;
+    cin >> n >> q;
+    lld arra[n+2];
+    arra[0] = 0;
+
+    for (i = 1; i <= n; i++)
+    {
+        cin >> ch;
+        arra[i]=arra[i-1]+((ch-'a')+1);
+    }
+    while (q--)
+    {
+        cin >> i >> j;
+        cout << arra[j]-arra[i-1] << endl;
+    }
+
+    return 0;
+}
